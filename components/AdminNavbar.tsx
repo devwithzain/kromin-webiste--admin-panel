c4cb2ea1d0014d64c947e4b435137b46002af1a1
devwithzain <ZainSoftwear11@gmail.com>
2024-07-12 02:58:03 +0500

    modified

--- components/AdminNavbar.tsx
+++ components/AdminNavbar.tsx
@@ -1,22 +1,25 @@
 import { TNavbarProps } from "@/types";
+import { UserMenu } from "@/components";
 import { CiSearch } from "react-icons/ci";
-import { UserMenu, Logo } from "@/components";
 
 export default function AdminNavbar({ currentUser }: TNavbarProps) {
 	return (
-		<div className="w-full padding-x py-4 border-b">
+		<div className="px-8 py-6">
 			<div className="w-full flex items-center justify-between gap-3">
-				<Logo />
-				<div className="flex px-5 py-[10px] items-center justify-between bg-[#F0F3F4] w-[25%] rounded-full">
+				<h1 className="text-[24px] font-ProximaNova font-semibold uppercase text-[#081226]">
+					Dashboard
+				</h1>
+				<div className="flex px-5 py-2 items-center justify-between bg-[#F0F3F4] w-[25%] rounded-full">
 					<input
 						type="text"
-						className="w-full text-[18px] outline-none bg-transparent border-none font-normal"
+						className="w-full text-[16px] outline-none bg-transparent border-none font-medium"
 						placeholder="Search..."
 					/>
 					<CiSearch size={30} />
 				</div>
 				<UserMenu currentUser={currentUser} />
 			</div>
+			<hr className="bg-black text-black w-full mt-2" />
 		</div>
 	);
 }
